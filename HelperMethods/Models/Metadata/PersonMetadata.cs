1b7421f2d2e635f709ab27ca35c9b70bb97689f8
srkneo <yadavmaheshing@gmail.com>
2016-02-06 14:28:35 +0530

    Max

---
+++ HelperMethods/Models/Metadata/PersonMetadata.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace HelperMethods.Models
+{
+    [DisplayName("New Person")]
+    public partial class PersonMetadata
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int PersonId { get; set; }
+
+        [Display(Name = "First")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Approved")]
+        public bool IsApproved { get; set; }
+    }
+}
--- HelperMethods/Models/Person.cs
+++ HelperMethods/Models/Person.cs
@@ -1,18 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel;
 
 namespace HelperMethods.Models
 {
-    public class Person
+    [MetadataType(typeof(PersonMetadata))]
+    public partial class Person
     {
+        [HiddenInput(DisplayValue=false)]
+        //[ScaffoldColumn(false)]
         public int PersonId { get; set; }
+
+        [Display(Name="First")]        
         public string FirstName { get; set; }
+
+        [Display(Name="Last")]
         public string LastName { get; set; }
+
+        [Display(Name="Birth Date")]
+        [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
+
         public Address HomeAddress { get; set; }
+
+        [Display(Name="Approved")]
         public bool IsApproved { get; set; }
+
+        [UIHint("Enum")]
         public Role Role { get; set; }
     }
 
